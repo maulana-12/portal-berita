3d375f93a1cbc02c94ae22772a7808511c8341c4
maulana-12 <mmaulana1202@gmail.com>
2023-07-04 17:15:43 +0700

    merapihkan index kategori

--- resources/views/back/category/index.blade.php
+++ resources/views/back/category/index.blade.php
@@ -5,11 +5,11 @@
     <div class="panel-header bg-primary-gradient">
         <div class="page-inner py-5">
             <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
-                <div>
+                {{-- <div>
                     <h2 class="text-white pb-2 fw-bold">Dashboard</h2>
                     <h5 class="text-white op-7 mb-2">Free Bootstrap 4 Admin Dashboard</h5>
                 </div>
-                {{-- <div class="ml-md-auto py-2 py-md-0">
+                <div class="ml-md-auto py-2 py-md-0">
                     <a href="#" class="btn btn-white btn-border btn-round mr-2">Manage</a>
                     <a href="#" class="btn btn-secondary btn-round">Add Customer</a>
                 </div> --}}
@@ -17,126 +17,7 @@
         </div>
     </div>
     <div class="page-inner mt--5">
-        <div class="row">
-            <div class="col-sm-6 col-md-3">
-                <div class="card card-stats card-round">
-                    <div class="card-body ">
-                        <div class="row align-items-center">
-                            <div class="col-icon">
-                                <div class="icon-big text-center icon-primary bubble-shadow-small">
-                                    <i class="fas fa-users"></i>
-                                </div>
-                            </div>
-                            <div class="col col-stats ml-3 ml-sm-0">
-                                <div class="numbers">
-                                    <p class="card-category">User</p>
-                                    <h4 class="card-title">isi</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="card card-stats card-round">
-                    <div class="card-body">
-                        <div class="row align-items-center">
-                            <div class="col-icon">
-                                <div class="icon-big text-center icon-info bubble-shadow-small">
-                                    <i class="far fa-newspaper"></i>
-                                </div>
-                            </div>
-                            <div class="col col-stats ml-3 ml-sm-0">
-                                <div class="numbers">
-                                    <p class="card-category">Article</p>
-                                    <h4 class="card-title">70</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="card card-stats card-round">
-                    <div class="card-body">
-                        <div class="row align-items-center">
-                            <div class="col-icon">
-                                <div class="icon-big text-center icon-success bubble-shadow-small">
-                                    <i class="fas fa-tags"></i>
-                                </div>
-                            </div>
-                            <div class="col col-stats ml-3 ml-sm-0">
-                                <div class="numbers">
-                                    <p class="card-category">Kategori</p>
-                                    <h4 class="card-title">80</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="card card-stats card-round">
-                    <div class="card-body">
-                        <div class="row align-items-center">
-                            <div class="col-icon">
-                                <div class="icon-big text-center icon-secondary bubble-shadow-small">
-                                    <i class="fas fa-file-video"></i>
-    
-                                </div>
-                            </div>
-                            <div class="col col-stats ml-3 ml-sm-0">
-                                <div class="numbers">
-                                    <p class="card-category">Video</p>
-                                    <h4 class="card-title">90</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card full-height">
-                    <div class="card-header">
-                        <div class="card-head-row">
-                            <div class="card-title">Materi Video</div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                    
-    
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card full-height">
-                    <div class="card-header">
-                        <div class="card-head-row">
-                            <div class="card-title">Playlist Video</div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        
-    
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card full-height">
-                    <div class="card-header">
-                        <div class="card-head-row">
-                            <div class="card-title">Draft Artikel</div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                    
-    
-                    </div>
-                </div>
-            </div>
-        </div>
+
         <div class="row">
             <div class="col-md-12">
                 <div class="card full-height">
@@ -147,7 +28,39 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
-                        
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">ID</th>
+                                        <th scope="col">Nama Kategori</th>
+                                        <th scope="col">Slug</th>
+                                        <th scope="col" style="width: 10%" class="text-center">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($categories as $row)
+                                        <tr>
+                                            <td>{{ $row->id }}</td>
+                                            <td>{{ $row->name }}</td>
+                                            <td>{{ $row->slug }}</td>
+                                            <td>
+                                                <div class="form-button-action">
+                                                    <button type="button" class="btn btn-link btn-primary btn-lg" >
+                                                        <i class="fa fa-edit"></i>
+                                                    </button>
+                                                    <button type="button" class="btn btn-link btn-danger">
+                                                        <i class="fa fa-times"></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <div>Data masih kosong</div>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
                         </div>
     
                     </div>
@@ -155,109 +68,7 @@
             </div>
         </div>
 
-        <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h4 class="card-title">Kategori</h4>
-                        <button class="btn btn-primary btn-round ml-auto" data-toggle="modal" data-target="#addRowModal">
-                            <i class="fa fa-plus"></i>
-                            Tambah Kategori
-                        </button>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <!-- Modal -->
-                    <div class="modal fade" id="addRowModal" tabindex="-1" role="dialog" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header no-bd">
-                                    <h5 class="modal-title">
-                                        <span class="fw-mediumbold">
-                                        New</span> 
-                                        <span class="fw-light">
-                                            Row
-                                        </span>
-                                    </h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <p class="small">Create a new row using this form, make sure you fill them all</p>
-                                    <form>
-                                        <div class="row">
-                                            <div class="col-sm-12">
-                                                <div class="form-group form-group-default">
-                                                    <label>Name</label>
-                                                    <input id="addName" type="text" class="form-control" placeholder="fill name">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6 pr-0">
-                                                <div class="form-group form-group-default">
-                                                    <label>Slug</label>
-                                                    <input id="addSlug" type="text" class="form-control" placeholder="fill position">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                                <div class="modal-footer no-bd">
-                                    <button type="button" id="addRowButton" class="btn btn-primary">Add</button>
-                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class="table-responsive">
-                        <table id="category" class="display table table-striped table-hover" >
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Slug</th>
-                                    <th style="width: 10%">Action</th>
-                                </tr>
-                            </thead>
-                            {{-- <tfoot>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Position</th>
-                                    <th>Office</th>
-                                    <th>Action</th>
-                                </tr>
-                            </tfoot> --}}
-                            <tbody>
-                                @forelse ($categories as $row)
-                                <tr>
-                                    <td>{{ $row->id }}</td>
-                                    <td>{{ $row->name }}</td>
-                                    <td>{{ $row->slug }}</td>
-                                    <td>
-                                        <div class="form-button-action">
-                                            <button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
-                                                <i class="fa fa-edit"></i>
-                                            </button>
-                                            <button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove">
-                                                <i class="fa fa-times"></i>
-                                            </button>
-                                        </div>
-                                    </td>
-                                </tr>
-                                @empty
-                                    <tr>
-                                        <td>Data masih kosong</td>
-                                    </tr>
-                                @endforelse
-                                
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 
